986cae38a54e0dc0b2aafa72ece1df304d241c30
Benjamin Miller <benmiller179@gmail.com>
2024-05-15 17:40:22 -0500

    Font, themes

--- config.el
+++ config.el
@@ -80,24 +80,12 @@
       ;; System clipboard.
       :desc "Paste from system clipboard" "s-v" #'paste-from-system-clipboard
       :desc "Copy to system clipboard" "s-c" #'copy-region-to-system-clipboard
-
-      ;; Old habits die hard.
-      :desc "Edit config" "s-," (lambda () (interactive) (edit-config-file "~/.doom.d/README.org"))
       )
 
 ;; Font.
-(setq doom-font (font-spec :family "Monaco" :size 20)
-      doom-variable-pitch-font (font-spec :family "Monaco" :size 20)
+(setq doom-font (font-spec :family "Monaco" :size 18)
+      doom-variable-pitch-font (font-spec :family "Monaco" :size 18)
       doom-big-font (font-spec :family "Monaco" :size 26))
-(after! doom-themes
-  (setq doom-themes-enable-bold t
-        doom-themes-enable-italic t))
-(custom-set-faces!
-  '(font-lock-comment-face :slant italic)
-  '(font-lock-keyword-face :slant italic))
-
-;; Theme.
-(setq doom-theme 'doom-one-light)
 
 ;; Disable line numbers.
 (setq display-line-numbers-type nil)
