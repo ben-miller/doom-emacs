e3ca833b9a574ceb66760485308ab393b1b946fe
Benjamin Miller <benmiller179@gmail.com>
2024-07-20 07:33:50 -0500

    Move editor settings to own file

---
+++ config/settings/editor.el
@@ -0,0 +1,54 @@
+(defun open-scratch-in-new-tab ()
+  "Open a new tab with a *scratch* buffer."
+  (interactive)
+  (tab-new)
+  (switch-to-buffer "*scratch*"))
+
+(defun split-and-balance-windows-vertically ()
+  (interactive)
+  (split-window-right)
+  (balance-windows)
+  (select-window (next-window)))
+
+(defun split-and-balance-windows-horizontally ()
+  (interactive)
+  (split-window-below)
+  (balance-windows)
+  (select-window (next-window)))
+
+(defun close-window-or-tab ()
+  (interactive)
+  (if (one-window-p)
+      (tab-close)
+    (progn
+      (delete-window)
+      (balance-windows))
+    ))
+
+(defun my-list-windows ()
+  "List all windows in the current tab along with their widths."
+  (interactive)
+  (let ((window-info '()))
+    (walk-windows
+     (lambda (w)
+       (push (format "%s (width: %d)" (buffer-name (window-buffer w)) (window-width w)) window-info))
+     nil t)
+    (message "Windows in current tab: %s" (mapconcat 'identity window-info ", "))))
+
+(defun window-is-maximized ()
+  "Check if any window in the current tab has a width under 16 characters."
+  (cl-some (lambda (w) (< (window-width w) 16))
+           (window-list)))
+
+(defun toggle-maximize-window ()
+  "Toggle the maximization state of the current window."
+  (interactive)
+  (if (window-is-maximized)
+      (balance-windows)    ; If the window is maximized, balance the windows.
+      (maximize-window)))  ; If the window is not maximized, maximize it.
+
+(defun move-and-maybe-maximize (move-fn)
+  "Move using the lambda function MOVE-FN and maximize if the window is already maximized."
+  (funcall move-fn)
+  (when (window-is-maximized)
+    (maximize-window)))
