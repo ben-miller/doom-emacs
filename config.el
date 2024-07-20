a7778d224d332ff46220870af2764ce8de140c03
Benjamin Miller <benmiller179@gmail.com>
2024-07-19 20:34:14 -0500

    Move centaur tabs config to own file

--- config.el
+++ config.el
@@ -228,42 +228,7 @@
 (after! neotree
   (setq neo-window-width 40))
 
-(defun centaur-tabs-buffer-groups ()
-  "Group buffers by their Projectile project."
-  (if (projectile-project-p)
-      (list (projectile-project-name))
-    (list "Misc")))
-
-(defun my/centaur-tabs-buffer-groups ()
-  (list
-   (cond
-    ((when-let ((project-name (centaur-tabs-project-name)))
-       project-name))
-    ((or (memq major-mode '(org-mode org-agenda-mode diary-mode))
-         (string-match-p "^\\*Org Agenda\\*" (buffer-name)))
-     "OrgMode")
-    ((or (string-equal "*" (substring (buffer-name) 0 1))
-         (memq major-mode '( magit-process-mode
-                             magit-status-mode
-                             magit-diff-mode
-                             magit-log-mode
-                             magit-file-mode
-                             magit-blob-mode
-                             magit-blame-mode)))
-     "Emacs")
-    ((derived-mode-p 'shell-mode) "Shell")
-    ((derived-mode-p 'eshell-mode) "EShell")
-    ((derived-mode-p 'emacs-lisp-mode) "Elisp")
-    ((derived-mode-p 'dired-mode) "Dired")
-    (t
-     (centaur-tabs-get-group-name (current-buffer))))))
-
-;; Apply the custom grouping function
-(setq centaur-tabs-buffer-groups-function #'my/centaur-tabs-buffer-groups)
-
-(setq centaur-tabs-cycle-scope 'tabs)
-
-(centaur-tabs-mode)
+(load (expand-file-name "centaur-tabs.el" (file-name-directory load-file-name)))
 
 ;; Magit
 (after! magit
