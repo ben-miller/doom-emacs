a74781bad1e20e65da33dcb867a4c4dd0ab9e9f7
Benjamin Miller <benmiller179@gmail.com>
2024-07-21 20:08:40 -0500

    Dead code

--- config/utils.el
+++
@@ -1,4 +0,0 @@
-(defun my/only-one-visible-tab-p ()
-  "Return t if there is only one visible tab in the current tab group."
-  (let ((tabs (centaur-tabs-view (centaur-tabs-current-tabset t))))
-    (eq (length tabs) 1)))
