adf1bd66af3be3b9cc4dc1415cb511974f94cd4b
Benjamin Miller <benmiller179@gmail.com>
2024-07-21 20:35:22 -0500

    Code format

--- config/packages/proj-persp.el
+++ config/packages/proj-persp.el
@@ -22,11 +22,12 @@
         projectile-require-project-root t
         projectile-switch-project-action (lambda ())
         projectile-track-known-projects-automatically nil)
-  (add-hook 'persp-switch-hook (lambda ()
-                                 (message (concat "persp name: " (prin1-to-string (persp-name (persp-curr)))))
-                                 (neotree-dir (cdr (assoc (persp-name (persp-curr)) persp-proj-map)))
-                                 (setq org-capture-templates (my/org-capture-templates)
-                                       org-agenda-files (my/org-agenda-files))))
+  (add-hook 'persp-switch-hook
+            (lambda ()
+              (message (concat "persp name: " (prin1-to-string (persp-name (persp-curr)))))
+              (neotree-dir (cdr (assoc (persp-name (persp-curr)) persp-proj-map)))
+              (setq org-capture-templates (my/org-capture-templates)
+                    org-agenda-files (my/org-agenda-files))))
   (add-hook 'projectile-after-switch-project-hook (lambda ())))
 
 (use-package! perspective
