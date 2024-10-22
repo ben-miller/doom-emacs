477cbfc0a2fa34f903f6688c55100a841052e288
Benjamin Miller <benmiller179@gmail.com>
2024-10-22 15:08:39 -0500

    End of line, start of line evil shortcuts

--- config/packages/evil.el
+++ config/packages/evil.el
@@ -1,9 +1,17 @@
-
 (after! evil
   (map! :n "s-/" nil)
+
   (setq evil-move-beyond-eol t)
   (setq evil-cross-lines t)
-  (setq evil-ex-search-case 'smart))
+  (setq evil-ex-search-case 'smart)
 
-(define-key evil-insert-state-map (kbd "s-<right>") 'centaur-tabs-forward)
-(define-key evil-insert-state-map (kbd "s-<left>") 'centaur-tabs-backward)
+  (map! :n "C-e" 'evil-end-of-line)
+  (map! :v "C-e" 'evil-end-of-line)
+  (map! :n "C-a" 'evil-beginning-of-line)
+  (map! :v "C-a" 'evil-beginning-of-line)
+
+  (define-key evil-insert-state-map (kbd "s-<right>") 'centaur-tabs-forward)
+  (define-key evil-insert-state-map (kbd "s-<left>") 'centaur-tabs-backward)
+
+  (define-key evil-insert-state-map (kbd "C-e") 'move-end-of-line)
+  )
